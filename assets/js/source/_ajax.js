2b50524a0e3600165553aeb36f89cd7c60cc2cf6
Chris Reynolds <chris.reynolds@mail.com>
2015-03-03 10:13:23 +0000

    Changed pagination styling

--- assets/js/source/_ajax.js
+++ assets/js/source/_ajax.js
@@ -2,79 +2,81 @@
 
   var buttonText;
 
-  function scroll_to_last_shout() {
-
-    var $wrapper  = $('#shoutbox .wrapper'),
-        $scrollTo = $wrapper.find('article:last-child');
-
-    $('#shoutbox .wrapper').animate({
-      scrollTop: $scrollTo.offset().top - $wrapper.offset().top + $wrapper.scrollTop()
-    });
-  }
-
-  function shoutbox_ajax() {
-
-    $.ajax({
-      type: 'POST',
-      dataType: 'html',
-      url: shoutboxObject.ajaxUrl,
-      data: { 
-        'action': 'ajax-shoutbox-get-new-post'
-      },
-      success: function(content) {
-
-        $('#shoutbox .wrapper').html(content);
-
-        $('#shoutbox-form .btn').removeAttr('disabled')
-                                .text(buttonText);
-
-        $('#shoutbox-form textarea').val('');
-
-      },
-      complete: function() {
-
-        scroll_to_last_shout();
-
-      }
-    });
-  }
-
   $(function() {
 
-    scroll_to_last_shout();
+    if ($('#shoutbox-form').length) {
 
-    $('#shoutbox-form').on('submit', function(e) {
+      function scroll_to_last_shout() {
 
-      e.preventDefault();
+        var $wrapper  = $('#shoutbox .wrapper'),
+            $scrollTo = $wrapper.find('article:last-child');
 
-      if ( $('#shoutbox-form textarea').val() ) {
+        $('#shoutbox .wrapper').animate({
+          scrollTop: $scrollTo.offset().top - $wrapper.offset().top + $wrapper.scrollTop()
+        });
+      }
+
+      function shoutbox_ajax() {
 
         $.ajax({
           type: 'POST',
-          dataType: 'json',
+          dataType: 'html',
           url: shoutboxObject.ajaxUrl,
           data: { 
-            'action':   'ajax-shoutbox-create-post',
-            'message':  $('#shoutbox-form textarea').val(),
-            'security': $('#shoutbox-form #security').val()
+            'action': 'ajax-shoutbox-get-new-post'
           },
-          beforeSend: function(data) {
+          success: function(content) {
 
-            buttonText = $('#shoutbox-form .btn').text();
+            $('#shoutbox .wrapper').html(content);
 
-            $('#shoutbox-form .btn').attr('disabled', 'disabled')
-                                    .text(shoutboxObject.loadingMessage);
+            $('#shoutbox-form .btn').removeAttr('disabled')
+                                    .text(buttonText);
+
+            $('#shoutbox-form textarea').val('');
+
           },
-          complete: function(data) {
+          complete: function() {
 
-            shoutbox_ajax();
+            scroll_to_last_shout();
 
           }
         });
       }
-    });
 
-    setInterval(shoutbox_ajax(), 1400);
+      scroll_to_last_shout();
 
+      $('#shoutbox-form').on('submit', function(e) {
+
+        e.preventDefault();
+
+        if ( $('#shoutbox-form textarea').val() ) {
+
+          $.ajax({
+            type: 'POST',
+            dataType: 'json',
+            url: shoutboxObject.ajaxUrl,
+            data: { 
+              'action':   'ajax-shoutbox-create-post',
+              'message':  $('#shoutbox-form textarea').val(),
+              'security': $('#shoutbox-form #security').val()
+            },
+            beforeSend: function(data) {
+
+              buttonText = $('#shoutbox-form .btn').text();
+
+              $('#shoutbox-form .btn').attr('disabled', 'disabled')
+                                      .text(shoutboxObject.loadingMessage);
+            },
+            complete: function(data) {
+
+              shoutbox_ajax();
+
+            }
+          });
+        }
+      });
+
+      setInterval(shoutbox_ajax(), 1400);
+    }
   });
 })(jQuery);
